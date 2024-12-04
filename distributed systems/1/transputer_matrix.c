01eae8a5353104aba1541a8e8425c5ffdb73258f
murlinmurlo <masha.burdugova@mail.ru>
2024-12-04 13:22:43 +0300

    minore changes

--- distributed systems/1/transputer_matrix.c
+++ distributed systems/1/transputer_matrix.c
@@ -5,76 +5,173 @@
 
 #define N_ROWS 5
 #define N_COLS 5
-#define NULL_RANK (-1)
+
 
 int size = N_ROWS * N_COLS;
 int rank;
 
-void decide_max(int *data, int *best_rank, int *other_data, int *other_rank) {
-    if (*other_data > *data) {
-        *data = *other_data;
-        *best_rank = *other_rank;
-    }
-}
 
-void communicate(int other0, int other1, int *recv_data, int *recv_rank, int *data, int *best_rank, int send, MPI_Comm comm) {
-    int other[2] = {other0, other1};
-    int other_rank;
+void communicate(int other_row, int other_col, 
+                 int *recv_data, int *recv_rank, 
+                 int *data, int *best_rank, 
+                 int send, MPI_Comm comm) {
     
-    // Проверка на допустимость координат
-    if (other[0] >= 0 && other[0] < N_ROWS && other[1] >= 0 && other[1] < N_COLS) {
-        MPI_Cart_rank(comm, other, &other_rank);
+    int other_coords[2] = {other_row, other_col};
+    int other_rank;
+    MPI_Cart_rank(comm, other_coords, &other_rank);
+
+    if (send) {
+        MPI_Send(best_rank, 1, MPI_INT, other_rank, 0, comm);
+        MPI_Send(data, 1, MPI_INT, other_rank, 0, comm);
+    } else {
+        MPI_Status status;
+        MPI_Recv(recv_rank, 1, MPI_INT, other_rank, 0, comm, &status);
+        MPI_Recv(recv_data, 1, MPI_INT, other_rank, 0, comm, &status);
         
-        if (send) {
-            MPI_Send(best_rank, 1, MPI_INT, other_rank, 0, comm);
-            MPI_Send(data, 1, MPI_INT, other_rank, 0, comm);
-        } else {
-            MPI_Recv(recv_rank, 1, MPI_INT, other_rank, 0, comm, MPI_STATUS_IGNORE);
-            MPI_Recv(recv_data, 1, MPI_INT, other_rank, 0, comm, MPI_STATUS_IGNORE);
-            decide_max(data, best_rank, recv_data, recv_rank);
+        if (*recv_data > *data) {
+            *data = *recv_data;
+            *best_rank = *recv_rank;
         }
     }
 }
 
-void reduction(int *data, int *best_rank, MPI_Comm comm, int coord[2]) {
-    int send = 1;
-    int other_data;
-    int other_rank;
 
-    // Проверка только на соседние процессы
-    if (coord[0] > 0) {
-        communicate(coord[0] - 1, coord[1], &other_data, &other_rank, data, best_rank, send, comm); // Верхний сосед
-    }
-    if (coord[0] < N_ROWS - 1) {
-        communicate(coord[0] + 1, coord[1], &other_data, &other_rank, data, best_rank, send, comm); // Нижний сосед
-    }
-    if (coord[1] > 0) {
-        communicate(coord[0], coord[1] - 1, &other_data, &other_rank, data, best_rank, send, comm); // Левый сосед
-    }
-    if (coord[1] < N_COLS - 1) {
-        communicate(coord[0], coord[1] + 1, &other_data, &other_rank, data, best_rank, send, comm); // Правый сосед
+void reduction(
+    int *data,
+    int *best_rank,
+    MPI_Comm comm,
+    int coord[2]
+) {
+    int send = 1;
+    int other_rank;
+    int other_data;
+    int row = coord[0];
+    int col = coord[1];
+
+    // Определяем направление для отправки и получения
+    int directions[2][2];
+
+    // Обработка по оси X
+    switch (row) {
+        case 0:
+        case 1:
+            if (row > 0) {
+                directions[0][0] = row - 1; directions[0][1] = col; // вверх
+                communicate(directions[0][0], directions[0][1], &other_data, &other_rank, data, best_rank, 0, comm);
+            }
+            directions[1][0] = row + 1; directions[1][1] = col; // вниз
+            communicate(directions[1][0], directions[1][1], &other_data, &other_rank, data, best_rank, send, comm);
+            break;
+
+        case 3:
+        case 4:
+            if (row < 4) {
+                directions[1][0] = row + 1; directions[1][1] = col; // вниз
+                communicate(directions[1][0], directions[1][1], &other_data, &other_rank, data, best_rank, 0, comm);
+            }
+            directions[0][0] = row - 1; directions[0][1] = col; // вверх
+            communicate(directions[0][0], directions[0][1], &other_data, &other_rank, data, best_rank, send, comm);
+            break;
+
+        default:
+            // Обработка по оси Y
+            directions[0][0] = row - 1; directions[0][1] = col; // вверх
+            directions[1][0] = row + 1; directions[1][1] = col; // вниз
+            for (int i = 0; i < 2; i++) {
+                communicate(directions[i][0], directions[i][1], &other_data, &other_rank, data, best_rank, 0, comm);
+            }
+
+            // Обработка по оси Y
+            directions[0][0] = row; directions[0][1] = col - 1; // влево
+            directions[1][0] = row; directions[1][1] = col + 1; // вправо
+
+            switch (col) {
+                case 0:
+                case 1:
+                    if (col > 0) {
+                        communicate(directions[0][0], directions[0][1], &other_data, &other_rank, data, best_rank, 0, comm);
+                    }
+                    communicate(directions[1][0], directions[1][1], &other_data, &other_rank, data, best_rank, send, comm);
+                    break;
+
+                case 3:
+                case 4:
+                    if (col < 4) {
+                        communicate(directions[1][0], directions[1][1], &other_data, &other_rank, data, best_rank, 0, comm);
+                    }
+                    communicate(directions[0][0], directions[0][1], &other_data, &other_rank, data, best_rank, send, comm);
+                    break;
+
+                default:
+                    for (int i = 0; i < 2; i++) {
+                        communicate(directions[i][0], directions[i][1], &other_data, &other_rank, data, best_rank, 0, comm);
+                    }
+                    break;
+            }
+            break;
     }
 }
+
+
 
 void broadcast(int *data, int *best_rank, MPI_Comm comm, int coord[2]) {
     int send = 1;
-    int other_data;
     int other_rank;
+    int other_data;
 
-    // Проверка только на соседние процессы
-    if (coord[0] > 0) {
-        communicate(coord[0] - 1, coord[1], &other_data, &other_rank, data, best_rank, !send, comm); // Верхний сосед
-    }
-    if (coord[0] < N_ROWS - 1) {
-        communicate(coord[0] + 1, coord[1], &other_data, &other_rank, data, best_rank, send, comm); // Нижний сосед
-    }
-    if (coord[1] > 0) {
-        communicate(coord[0], coord[1] - 1, &other_data, &other_rank, data, best_rank, send, comm); // Левый сосед
-    }
-    if (coord[1] < N_COLS - 1) {
-        communicate(coord[0], coord[1] + 1, &other_data, &other_rank, data, best_rank, send, comm); // Правый сосед
+    switch (coord[0]) {
+        case 0:
+        case 1:
+            // Передача вниз
+            communicate(coord[0] + 1, coord[1], &other_data, &other_rank, data, best_rank, !send, comm);
+            if (coord[0] > 0) {
+                // Передача вверх
+                communicate(coord[0] - 1, coord[1], &other_data, &other_rank, data, best_rank, send, comm);
+            }
+            break;
+
+        case 3:
+        case 4:
+            // Передача вверх
+            communicate(coord[0] - 1, coord[1], &other_data, &other_rank, data, best_rank, !send, comm);
+            if (coord[0] < 4) {
+                // Передача вниз
+                communicate(coord[0] + 1, coord[1], &other_data, &other_rank, data, best_rank, send, comm);
+            }
+            break;
+
+        default:
+            // Обработка по горизонтали (осе Y)
+            switch (coord[1]) {
+                case 0:
+                case 1:
+                    communicate(coord[0], coord[1] + 1, &other_data, &other_rank, data, best_rank, !send, comm);
+                    if (coord[1] > 0) {
+                        communicate(coord[0], coord[1] - 1, &other_data, &other_rank, data, best_rank, send, comm);
+                    }
+                    break;
+
+                case 3:
+                case 4:
+                    communicate(coord[0], coord[1] - 1, &other_data, &other_rank, data, best_rank, !send, comm);
+                    if (coord[1] < 4) {
+                        communicate(coord[0], coord[1] + 1, &other_data, &other_rank, data, best_rank, send, comm);
+                    }
+                    break;
+
+                default:
+                    // Передача по обеим осям
+                    communicate(coord[0], coord[1] - 1, &other_data, &other_rank, data, best_rank, send, comm);
+                    communicate(coord[0], coord[1] + 1, &other_data, &other_rank, data, best_rank, send, comm);
+                    break;
+            }
+            // Передача по вертикали
+            communicate(coord[0] - 1, coord[1], &other_data, &other_rank, data, best_rank, send, comm);
+            communicate(coord[0] + 1, coord[1], &other_data, &other_rank, data, best_rank, send, comm);
+            break;
     }
 }
+
 
 int main(int argc, char *argv[]) {
     MPI_Init(&argc, &argv);
@@ -105,7 +202,7 @@ int main(int argc, char *argv[]) {
 
     reduction(&data, &best_rank, comm, coords);
     MPI_Barrier(comm);
-    if (rank == 0) { printf("\nMax data value: %d\nMax data rank: %d\n", data, best_rank); }
+    if (coords[0] == 2 && coords[1] == 2) { printf("\nMax data value: %d\nMax data rank: %d\n", data, best_rank); }
 
     broadcast(&data, &best_rank, comm, coords);
     int best_coords[2];
