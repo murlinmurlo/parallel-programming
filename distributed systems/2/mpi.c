7dcb62e055fc9d3aa1feb317cb48727c312e2a93
murlinmurlo <masha.burdugova@mail.ru>
2024-12-12 21:29:30 +0300

    errhandler

--- distributed systems/2/mpi.c
+++ distributed systems/2/mpi.c
@@ -74,7 +74,8 @@ int main(int argc, char **argv) {
     int it, rank, size;
     MPI_Init(&argc, &argv);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank); 
-    MPI_Comm_size(MPI_COMM_WORLD, &size); 
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+    MPI_Comm_set_errhandler(MPI_COMM_WORLD, MPI_ERRORS_RETURN);
     MPI_Status status;
     struct timeval starttime, stoptime;
     gettimeofday(&starttime, NULL);
